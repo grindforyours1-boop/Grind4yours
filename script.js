f441c3f9f6bd1e7d5c3cb39578513fe4c67be054
grindforyours1-boop <grindforyours1@gmail.com>
2026-06-10 22:02:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+console.log("GrindForYours Loaded");
 
+document.querySelector("form").addEventListener("submit", function(e){
+e.preventDefault();
+
+alert("Thank you for contacting GrindForYours!");
+});
